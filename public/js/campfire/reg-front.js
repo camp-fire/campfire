1fe83511f8796ecb5f83c33574387d63b80c5ab6
camp-fire <camp_fire@foxmail.com>
2014-11-05 22:54:28 +0800

    test

--- public/js/campfire/reg-front.js
+++ public/js/campfire/reg-front.js
@@ -1,11 +1,11 @@
-(function($){
-    $(function(){
-        $("#regBtn").bind("click",function(event){
-            var password = $("#password").val();
-            var confirmPassword = $("#confirmPassword").val();
-            if(password !== confirmPassword){
-            	alert("密码不等");
-            }
-        });
-    });
+(function($) {
+	$(function() {
+		$("#regBtn").bind("click", function(event) {
+			var password = $("#password").val();
+			var confirmPassword = $("#confirmPassword").val();
+			if (password !== confirmPassword) {
+				alert("密码不等");
+			}
+		});
+	});
 })(jQuery);
